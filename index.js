646cede5639e4a0c1ccef870cb132d76c0a32d93
Frank Siao <fsiao@appnexus.com>
2015-04-05 19:52:18 -0400

    uploads and runs contracts

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 var express = require("express");
-var app = express();
 var mysql = require('mysql');
 var multer = require('multer');
+var fs = require('fs');
+var csvtojson = require("csvtojson");
+var _ = require("underscore");
+
 var config = require('./config.json');
 
-console.log(config);
+var app = express();
+
+//promise helpers
+var Promise = require('promise');
+var pReadFile = Promise.denodeify(fs.readFile);
+
 app.use(express.static(__dirname + '/public'));
+app.use(multer({
+	dest: './uploads/',
+	rename: function (fieldname, filename) {
+		return filename.replace(/\W+/g, '-').toLowerCase() + Date.now();
+	},
+	onFileUploadComplete: function(file, req, res) {
+		console.log(file.fieldname + ' uploaded to  ' + file.path);
+	}
+}));
 
 //Database stuff
 app.get('/contracts', function(req, res, next) {
@@ -50,28 +67,146 @@ var connection = mysql.createConnection({
 connection.connect();
 
 function insertNewContract(contractName) {
-	console.log('Inserting new contract named: ' + contractName);
-	connection.query('INSERT INTO opt.tblContracts (Name) VALUES(?)', [contractName], function(err, result) {
-		if (!err) {
-			console.log('Inserted ' + result.insertId);
-			//TODO: return promise
-		} else {
-			console.log('Error while performing Query.');
-			console.log(err);
-		}
-	});
+	return function(contractData) {
+		var promise = new Promise(function(resolve,reject) {
+			console.log('Inserting new contract named: ' + contractName);
+			var query = connection.query('INSERT INTO opt.tblContracts (Name) VALUES(?)', [contractName], function(err, result) {
+				if (!err) {
+					console.log('Inserted ' + result.insertId);
+					resolve({
+						name: contractName,
+						id: result.insertId,
+						data: contractData
+					});
+				} else {
+					console.log('Error while performing Query.');
+					console.log(err);
+					reject(err);
+				}
+			});
+			console.log(query.sql);
+		});
+		return promise;
+	};
 };
 
-function populateSimulations(simulations) {
-	//columns for tblSimulations: ContractSID,Year,Event,Loss 
-	//TODO: figure out how to insert into opt.tblSimulations
+function populateSimulations(params) {
+	var promise = new Promise(function(resolve,reject) {
+		console.log('Populating simulations');
+		var data = formatSimulations(params.data, params.id);
+		var query = 'INSERT INTO opt.tblSimulations (ContractSID, Year, Event, Loss) Values ?';
+		var q = connection.query(query, [data], function(err, result) {
+			if (err) {
+				reject(err);
+			} else {
+				console.log(result);
+				console.log('Loaded simulations');
+				resolve();
+			}
+		});
+		console.log(q.sql);
+		console.log(q.values);
+	});
+	return promise;
 }
 
+function clearTable(tbl) {
+	return function() {
+		var promise = new Promise(function(resolve,reject) {
+			console.log('Clearing table ', tbl);
+			var q = connection.query('TRUNCATE TABLE ' + tbl, function(err, result) {
+				if (err) {
+					reject(err);
+				} else {
+					console.log('Cleared');
+					console.log(result);
+					resolve();
+				}
+			});
+			console.log(q.sql);
+		});
+		return promise;
+	}
+}
+
+function deleteFile(path) {
+	return function() {
+		console.log('Deleting: ', path);
+		var promise = new Promise(function(resolve, reject) {
+			fs.unlink(path, function(err) {
+				if (err) {
+					reject(err);
+				} else {
+					resolve();
+				}
+			});
+		});
+		return promise;
+	}
+}
+
+function formatSimulations(simulations, id) {
+	var formatted = [];
+	_.each(simulations, function(value, key) {
+		formatted.push([
+			id,
+			value.Year,
+			value.Event,
+			value.Loss
+		]);
+	});
+	return formatted;
+}
 //TODO:Figure out if I even need to end the connection
 //connection.end();
 
-//TODO: handle uploading
-//http://codeforgeek.com/2014/11/ajax-file-upload-node-js/
+function convertToJson(data) {
+	var promise = new Promise(function(resolve,reject) {
+		var converter = new csvtojson.core.Converter();
+		converter.fromString(data, function(err, json) {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(json);
+			}
+		});
+	});
+	return promise;
+}
 
+app.post('/upload-object', function(req, res) {
+	console.log('Uploading');
+	var path = req.files.contract_file.path;
+	var contractName = req.body.contract_name;
+	console.log(contractName);
+	pReadFile(path, 'utf8')
+	.then(convertToJson)
+	.then(insertNewContract(contractName))
+	.then(populateSimulations)
+	.then(clearTable('opt.tblResult'))
+	.then(deleteFile(path))
+	.then(handUploadSuccess, handleUploadFailure);
+	function handUploadSuccess(data) {
+		res.send('success');
+	}
+	function handleUploadFailure(err) {
+		console.log(err);
+		res.send('failure');
+	}
 
+});
+app.post('/clear-all', function(req, res) {
+	console.log('clear-all');
+	clearTable('opt.tblResult')()
+	.then(clearTable('opt.tblContracts'))
+	.then(clearTable('opt.tblSimulations'))
+	.then(handClearSuccess, handleClearFailure);
+	function handClearSuccess(data) {
+		res.send('clear success');
+	}
+	function handleClearFailure(err) {
+		console.log(err);
+		res.send('clear failure');
+	}
+});
 app.listen(3001);
--- public/js/main.js
+++ public/js/main.js
@@ -1,48 +1,56 @@
-console.log('main.js');
+$(document).ready(function() {
 
-$.get( "contracts", function(data) {
-	console.log(data);
-	$('.contract-code').append(JSON.stringify(data, null, 2));
-});
+	var $contractNameGroup = $('#contract-name-group');
 
-$.get( "results", function(data) {
-	console.log(data);
-	$('.result-code').append(JSON.stringify(data, null, 2));
-});
+	$("#data-file-upload").fileinput({
+		allowedFileExtensions: ["csv"],
+		showPreview: false,
+		elErrorContainer: "#file-type-error"
+	});
+	$.get( "contracts", function(data) {
+		console.log(data);
+		$('.contract-code').append(JSON.stringify(data, null, 2));
+	});
 
-$('#upload-button').on('click', function(e) {
-	console.log('button clicked');
-	var file = $('#file-input')[0].files[0];
-	var data = new FormData();
-	data.append('file1',file);
-	console.log(data);
+	$.get( "results", function(data) {
+		console.log(data);
+		$('.result-code').append(JSON.stringify(data, null, 2));
+	});
 
-	var reader = new FileReader();
-	reader.onload = function(e) {
-		var text = reader.result;
-
-		$.ajax({
-			url: 'create-object',
-			method: 'POST',
-			cache: false,
-			content: data,
-			contentType: false,
-    		processData: false,
-			// contentType: 'multipart/form-data'
+	$('#run-r').on('click', function(e) {
+		console.log('run');
+		$.post('//ec2-52-0-86-122.compute-1.amazonaws.com/ocpu/library/opt/opt_test.R', function(data) {
+			console.log('post return');
 		}).done(function() {
 			console.log('done');
-		}).fail(function() {
-			console.log('fail');
 		});
-	}
-	reader.readAsText(file);
-});
-
-$('#run-r').on('click', function(e) {
-	console.log('run');
-	$.post('//ec2-52-0-86-122.compute-1.amazonaws.com/ocpu/library/opt/opt_test.R', function(data) {
-		console.log('post return');
-	}).done(function() {
-		console.log('done');
 	});
+
+	$('#clear-all').on('click', function(e) {
+		console.log('clear all');
+		$.post('clear-all', function() {
+			console.log('cleared');
+		});
+	});
+
+	$('#uploadForm').ajaxForm({
+		beforeSubmit: function(formData, jqForm, options) {
+			if (formData[0].value) {
+				return true;
+			} else {
+				$contractNameGroup.addClass('has-error');
+				$contractNameGroup.find('.contract-error').show();
+				return false;
+			}
+		},
+		success: function(responseText, statusText) {
+			console.log(responseText);
+		}
+	});
+
+	$('#new-contract-name').on('blur', function() {
+		$contractNameGroup.removeClass('has-error');
+		$contractNameGroup.find('.contract-error').hide();
+	});
+
 });
